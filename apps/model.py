d4b822c84c61ddb26757170485a7f007c24ae070
YasumasaTakemura <y.takemura@datumstudio.jp>
2018-04-25 16:55:17 +0900

    int

--- apps/model.py
+++ apps/model.py
@@ -1,9 +1,36 @@
+import os
+import re
+import cloudstorage
+from google.appengine.ext import ndb
+from bs4 import BeautifulSoup
 
-import hashlib
 
-class Hash(object):
-    def __init__(self,key):
-        self.key = key
+class UrlStateModel(ndb.Model):
+    path = ndb.StringProperty()
+    created_at = ndb.DateTimeProperty(auto_now_add=True)
 
-    def get_hash(self):
-        return hashlib.sha256(self.key).hexdigest()
+
+class UrlState(UrlStateModel):
+    @staticmethod
+    def reverse_id(key):
+        return key[::-1]
+
+
+    @classmethod
+    def get_one(cls, p):
+        key = cls.reverse_id(p)
+        return cls.get_by_id(key)
+
+
+def read_file_from_gcs(filename):
+    with cloudstorage.open(filename) as f:
+        f.seek(-1024, os.SEEK_END)
+        data = f.read()
+    return data
+
+
+def parse(data):
+    html = BeautifulSoup(data)
+    word = '/Restaurant'
+    res = html.find_all(href=re.compile(word))
+    return res
--- apps/view.py
+++ apps/view.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from flask import Blueprint, request, jsonify
-from .model import Hash
+from .model import UrlState, read_file_from_gcs, parse
 
 app = Blueprint('hello', __name__)
 
@@ -11,22 +11,26 @@ def hello():
     return 'hello, world', 200
 
 
-@app.route('/json', methods=['GET', 'POST'])
-def json():
+@app.route('/push', methods=['GET', 'POST'])
+def push():
+    """ extract files from GCS """
     data = request.json
-    return jsonify(data=data), 200
+    filename = data['message']['bucketId'] + '/' + data['message']['objectId']
+    data = read_file_from_gcs(filename)
+
+    # transform /parse files
+    path = parse(data)
+
+    # load items
+    for p in path:
+        state = UrlState(**{'path': p})
+        state.put()
+
+    return 'success', 200
 
 
-@app.route('/arg', methods=['GET'])
+@app.route('/get_one', methods=['GET'])
 def arg():
-    key = request.args.get("key", type=str)
-    hash_obj = Hash(key)
-    val = hash_obj.get_hash()
-    return val, 200
-
-
-@app.route('/form', methods=['POST'])
-def form():
-    req = request.form
-    val = req.get('key')
-    return jsonify(data=val), 200
+    key = request.args.get("p", type=str)
+    path = UrlState.get_one(key)
+    return path, 200
